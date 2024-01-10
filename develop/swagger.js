bd5ccf6e814753480eccd87ba9ffe7c4ff4007f3
Alex Liu <alex_liu@migocorp.com>
2024-01-10 11:32:32 +0800

    feat: 調整 swagger.js

--- develop/swagger.js
+++ develop/swagger.js
@@ -3,16 +3,8 @@ import path from 'path';
 
 const doc = {
     info: {
-        title: 'Freyja 旅館 API 系統 | 六角學院',
-        description: `打破影音課程售後不理，我們在線上等著你。\n注意事項：登入成功後請點「Authorize」輸入 Token。\n\n範例程式碼 :
-
-    fetch('/api/v1/home/news', { method: 'GET' })
-        .then(response => response.json())
-        .then(res => {
-            // { status: 'true', result: [{...}] }
-            console.log(res);
-        });
-    `
+        title: 'ZenStay API',
+        description: 'Cozy Inn'
     },
     securityDefinitions: {
         bearerAuth: {
